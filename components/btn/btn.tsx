6c533f5066b197ab39608a1073f2ebe173856355
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-18 15:41:11 +0100

    💄 Add button styles

--- components/btn/btn.tsx
+++ components/btn/btn.tsx
@@ -1,11 +1,15 @@
-import styles from "components/btn/btn.module.css";
+import styles from "./btn.stories";
 
 export type BtnProps = {
   label: string;
 };
 
 function Btn({ label, ...props }: BtnProps) {
-  return <button {...props}>{label}</button>;
+  return (
+    <button className={styles.btn} {...props}>
+      {label}
+    </button>
+  );
 }
 
 export default Btn;
