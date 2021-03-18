41223ac6e6f7a92ac551ae297759f390618a0721
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-18 15:25:19 +0100

    ✨ Add Btn stories

--- components/btn/btn.stories.tsx
+++ components/btn/btn.stories.tsx
@@ -1,3 +1,20 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import Btn, { BtnProps } from "./btn";
+
+export default {
+  title: "Components/Btn",
+  component: Btn,
+} as Meta;
+
+const Template: Story<BtnProps> = (args) => <Btn {...args} />;
+
+export const Calculate = Template.bind({});
+Calculate.args = {
+  label: "Calculate >>>",
+};
+
+export const Add = Template.bind({});
+Add.args = {
+  label: "Add 💚",
+};
