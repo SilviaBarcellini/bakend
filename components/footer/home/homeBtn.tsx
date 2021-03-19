28b3113e189b622194b230eb6cd8e1550115f971
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-19 19:50:35 +0100

    💄 style home component

--- components/footer/home/homeBtn.tsx
+++ components/footer/home/homeBtn.tsx
@@ -6,7 +6,7 @@ const homeImg = (home) => {
 
 function HomeBtn() {
   return (
-    <a className={styles.homeBtn} href="http://www.google.com/">
+    <a className={styles.homeBtn} href="http://localhost:3000/">
       <img
         className={styles.homeImg}
         src="https://www.flaticon.com/svg/vstatic/svg/1946/1946436.svg?token=exp=1616180196~hmac=c6fda90f786ed79a878546f959dc74cc"
