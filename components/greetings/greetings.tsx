602a4fc815e29cba4098b99f7cc3cf981d06da3a
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-19 15:39:52 +0100

    ✨ add greeting options

--- components/greetings/greetings.tsx
+++ components/greetings/greetings.tsx
@@ -4,8 +4,20 @@ import styles from "./greetings.module.css";
 const date = new Date();
 const currentTime = date.getHours();
 
+let greetings;
+
+if (currentTime < 12) {
+  greetings = "Good Morning";
+} else if (currentTime < 18) {
+  greetings = "Good Afternoon";
+} else if (currentTime < 22) {
+  greetings = "Good Evening";
+} else {
+  greetings = "Good Night";
+}
+
 function Greetings() {
-  return <h2 className={styles.greetings}>Hello Joan!</h2>;
+  return <h2 className={styles.greetings}>{greetings} Joan!</h2>;
 }
 
 export default Greetings;
