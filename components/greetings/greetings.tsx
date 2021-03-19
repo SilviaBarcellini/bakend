ce5d48ae93abe8a126972bdefff66608fd13a41b
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-19 15:33:59 +0100

    ✨ add .getHours()

--- components/greetings/greetings.tsx
+++ components/greetings/greetings.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./greetings.module.css";
 
+const date = new Date();
+const currentTime = date.getHours();
+
 function Greetings() {
   return <h2 className={styles.greetings}>Hello Joan!</h2>;
 }
