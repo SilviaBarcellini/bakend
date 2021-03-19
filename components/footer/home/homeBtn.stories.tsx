73c26c4771851c3b0f62e5f004f2d0a5b91e9544
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-19 21:12:48 +0100

    ♻️ refactor home component code

--- components/footer/home/homeBtn.stories.tsx
+++ components/footer/home/homeBtn.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { HomeBtnProps } from "./homeBtn";
 import homeBtn from "./home.png";
 
 export default {
   title: "homeBtn",
-};
+  component: homeBtn,
+} as Meta;
 
 const image = {
   src: homeBtn,
--- components/footer/home/homeBtn.tsx
+++ components/footer/home/homeBtn.tsx
@@ -1,11 +1,19 @@
 import styles from "./homeBtn.module.css";
+import homeBtn from "./home.png";
+import homeImg from "./homeBtn.stories";
 
-function HomeBtn() {
+export type HomeBtnProps = {
+  src: string;
+};
+
+function HomeBtn({ src, ...props }: HomeBtnProps) {
   return (
-    <a className={styles.homeBtn} href="http://localhost:3000/">
+    <a href="http://localhost:3000/">
       <img
-        className={styles.homeImg}
-        src="https://www.flaticon.com/svg/vstatic/svg/1946/1946436.svg?token=exp=1616180196~hmac=c6fda90f786ed79a878546f959dc74cc"
+        className={styles.homeBtn}
+        {...props}
+        src="https://www.flaticon.com/svg/vstatic/svg/1946/1946436.svg?token=exp=1616184268~hmac=26a662933dec7e3109c54f9645dfdf0a"
+        alt="takeme-home"
       />
     </a>
   );
