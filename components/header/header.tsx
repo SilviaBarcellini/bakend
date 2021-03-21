a6224b916908c2d1c9376a5b58fe8437934710ce
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-21 21:29:10 +0100

    ✨ add header component

---
+++ components/header/header.tsx
@@ -0,0 +1,19 @@
+import styles from "./header.module.css";
+
+export type HeaderProps = {
+  logo: string;
+  imageSrc: string;
+};
+
+function Header({ logo, imageSrc, ...props }: HeaderProps) {
+  return (
+    <div className={styles.header}>
+      <h1 className={`${styles.logo}`} {...props}>
+        {logo}
+      </h1>
+      <img className={styles.donut} src={imageSrc} alt="donut" />
+    </div>
+  );
+}
+
+export default Header;
