23117f99cc9d529c108f1906b3a015f01ddfe07d
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-21 21:30:15 +0100

    ✨ add header component stories

--- components/header/header.stories.tsx
+++ components/header/header.stories.tsx
@@ -1,2 +1,17 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+
+import Header, { HeaderProps } from "./header";
+
+export default {
+  title: "Common/Header",
+  component: Header,
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+
+export const header = Template.bind({});
+header.args = {
+  logo: "bakend",
+  imageSrc: "https://image.flaticon.com/icons/png/128/3190/3190845.png",
+};
