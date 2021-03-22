b758f724879d87f48c0c90eec3d0075781dc11a2
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-22 18:58:41 +0100

    ✨ call open page function to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,12 @@ import Greetings from "../components/greetings/greetings";
 import styles from "../styles/Home.module.css";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import OpenNewPage from "../components/openNewPage/openNewPage";
 
 export default function Home() {
   return (
     <div>
-      <Header
-        logo="bakend"
-        //imageSrc="https://image.flaticon.com/icons/png/128/3190/3190845.png"
-        imageSrc="/donuts.svg"
-      />
+      <Header logo="bakend" imageSrc="/donuts.svg" />
       <div>
         <div className={styles.container}>
           <Head>
@@ -24,6 +21,7 @@ export default function Home() {
           <main className={styles.main}>
             <Button primary label="Primary" />
             <Button primary={false} label="Secondary" />
+            <OpenNewPage />
           </main>
           <div>
             <Greetings />
