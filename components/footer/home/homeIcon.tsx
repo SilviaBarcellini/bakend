e3cd30b28db4d8510c599bcf787bfafca7648b08
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-22 12:38:24 +0100

    ✨ update Home Icon component

--- components/footer/home/homeIcon.tsx
+++ components/footer/home/homeIcon.tsx
@@ -1,1 +1,15 @@
-import styles from "./homeBtn.module.css";
+import styles from "./homeIcon.module.css";
+
+export type HomeIconProps = {
+  imgSrc: string;
+};
+
+function HomeIcon({ imgSrc }: HomeIconProps) {
+  return (
+    <a href="#">
+      <img className={styles.icon} src={imgSrc} />
+    </a>
+  );
+}
+
+export default HomeIcon;
