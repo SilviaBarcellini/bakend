b7367720f17d3cbb1ba4286f7c7d9c73a7b6ad1c
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-23 09:42:39 +0100

    ✨ add splash screen

--- pages/splash.tsx
+++ pages/splash.tsx
@@ -1,9 +1,17 @@
 import styles from "../styles/Splash.module.css";
+import Logo from "../components/logo/logo";
 
 export default function SplashPage() {
   return (
-    <div className={styles.main}>
-      <a href="http://localhost:3000">back</a>
+    <div>
+      <div className={styles.logo}>
+        <Logo logo="bakend" size="big" />
+      </div>
+      <div>
+        <a className={styles.link} href="http://localhost:3000">
+          Press here to continue
+        </a>
+      </div>
     </div>
   );
 }
