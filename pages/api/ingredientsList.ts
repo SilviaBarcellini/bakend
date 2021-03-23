323ad767dbd100ee554ac66d63df7043a4dd5fe2
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-23 14:37:47 +0100

    ✨ add req and res from api

---
+++ pages/api/ingredientsList.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withDatabase, ingredientsList } from "../../server/db";
+
+export default withDatabase(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const list = await ingredientsList("Ingredients");
+    res.status(200).json(list);
+  }
+);
