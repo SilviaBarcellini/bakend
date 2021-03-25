a28fbb23fdedf4288fb74b6c873cebdf70995cc2
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-25 23:57:37 +0100

    ♻️  add useEffect to fecth data from API

--- components/forms/form.tsx
+++ components/forms/form.tsx
@@ -4,6 +4,19 @@ import { useState } from "react";
 import { getIngredient } from "../../src/utils/api";
 
 function Form() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
+  const getIngredients = async () => {
+    const response = await fetch(`/api/ingredients`);
+    const ingredients = await response.json();
+    setIngredients(ingredients);
+    console.log(ingredients);
+  };
+
   return (
     <div className={styles.container}>
       <form className={styles.form}>
