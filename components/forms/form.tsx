f948f9a304165f4d101f8f9a6ef2fe537e3344bf
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-26 01:09:18 +0100

    ♻️ inject Hook Form in code

--- components/forms/form.tsx
+++ components/forms/form.tsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import styles from "./form.module.css";
 //import { getIngredient } from "../../src/utils/api";
 import CardMain from "../cardMain/cardMain";
 
+type FormProps = {
+  itemRequired: string;
+};
+
 function Form() {
+  const { register, handleSubmit, errors } = useForm<FormProps>();
+  const onSubmit: SubmitHandler<FormProps> = (data) => {
+    alert(data);
+  };
   const [ingredients, setIngredients] = useState([]);
   const [filter, setFilter] = useState("");
+  const [query, setQuery] = useState("sugar");
 
   useEffect(() => {
     getIngredients();
-  }, []);
+  }, [query]);
 
   const getIngredients = async () => {
-    const response = await fetch(`/api/ingredients`);
+    const response = await fetch(`/api/ingredients/`);
     const ingredients = await response.json();
     setIngredients(ingredients);
     console.log(ingredients);
@@ -22,15 +32,22 @@ function Form() {
     setFilter(e.target.value);
   };
 
+  const getSearch = (e) => {
+    setQuery(filter);
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
         <input
           className={styles.input}
+          name="itemRequired"
+          ref={register({ required: true, minLength: 4 })}
           type="text"
           value={filter}
           onChange={updateSearch}
         />
+        {errors.itemRequired && <p>This field is required</p>}
         <button className={styles.button} type="submit">
           Go!
         </button>
