7e08fbe3337b3709b294b4d110c510ad005188f8
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-29 16:02:23 +0200

    📱 delete cards container

--- components/cardMain/cardMain.tsx
+++ components/cardMain/cardMain.tsx
@@ -30,67 +30,65 @@ function CardMain({
   soyFree,
 }: CardMainProps) {
   return (
-    <div className={styles.cardsContainer}>
-      <div className={styles.cardMain}>
-        <div className={styles.cardIntro}>
-          <h2 className={styles.name}>{name}</h2>
-          <h3>{family}</h3>
-          <img className={styles.image} src={image} alt="" />
+    <div className={styles.cardMain}>
+      <div className={styles.cardIntro}>
+        <h2 className={styles.name}>{name}</h2>
+        <h3>{family}</h3>
+        <img className={styles.image} src={image} alt="" />
+      </div>
+      <div className={styles.factsContainer}>
+        <div className={styles.factContainer}>
+          <h3 className={styles.factTitle}>
+            % Fat
+            <p className={styles.factContent}>{fat}</p>
+          </h3>
         </div>
-        <div className={styles.factsContainer}>
-          <div className={styles.factContainer}>
-            <h3 className={styles.factTitle}>
-              % Fat
-              <p className={styles.factContent}>{fat}</p>
-            </h3>
-          </div>
-          <div className={styles.factContainer}>
-            <h3 className={styles.factTitle}>
-              % Carbs<p className={styles.factContent}>{carbs}</p>
-            </h3>
-          </div>
-          <div className={styles.factContainer}>
-            <h3 className={styles.factTitle}>
-              % Protein
-              <p className={styles.factContent}>{protein}</p>
-            </h3>
-          </div>
+        <div className={styles.factContainer}>
+          <h3 className={styles.factTitle}>
+            % Carbs<p className={styles.factContent}>{carbs}</p>
+          </h3>
         </div>
-        <div>
-          <div className={styles.info}>
-            <h4 className={styles.diet}>
-              <span>{vegan === true ? "💚 " : "💔"}</span>
-              Vegan
-            </h4>
-            <h4 className={styles.diet}>
-              <span>{vegetarian === true ? "💚 " : "💔"}</span>
-              Vegetarian
-            </h4>
-            <h4 className={styles.gluten}>
-              <span className={styles.yes}>
-                {glutenFree === true ? "💚 " : "💔"}
-              </span>
-              Gluten Free
-            </h4>
-            <h4 className={styles.dairy}>
-              <span className={styles.span}>
-                {dairyFree === true ? "💚 " : "💔"}
-              </span>
-              Dairy Free
-            </h4>
-            <h4 className={styles.nuts}>
-              <span className={styles.span}>
-                {nutsFree === true ? "💚 " : "💔"}
-              </span>
-              Nuts Free
-            </h4>
-            <h4 className={styles.soy}>
-              <span className={styles.span}>
-                {soyFree === true ? "💚 " : "💔"}
-              </span>
-              Soy Free
-            </h4>
-          </div>
+        <div className={styles.factContainer}>
+          <h3 className={styles.factTitle}>
+            % Protein
+            <p className={styles.factContent}>{protein}</p>
+          </h3>
+        </div>
+      </div>
+      <div>
+        <div className={styles.info}>
+          <h4 className={styles.diet}>
+            <span>{vegan === true ? "💚 " : "💔"}</span>
+            Vegan
+          </h4>
+          <h4 className={styles.diet}>
+            <span>{vegetarian === true ? "💚 " : "💔"}</span>
+            Vegetarian
+          </h4>
+          <h4 className={styles.gluten}>
+            <span className={styles.yes}>
+              {glutenFree === true ? "💚 " : "💔"}
+            </span>
+            Gluten Free
+          </h4>
+          <h4 className={styles.dairy}>
+            <span className={styles.span}>
+              {dairyFree === true ? "💚 " : "💔"}
+            </span>
+            Dairy Free
+          </h4>
+          <h4 className={styles.nuts}>
+            <span className={styles.span}>
+              {nutsFree === true ? "💚 " : "💔"}
+            </span>
+            Nuts Free
+          </h4>
+          <h4 className={styles.soy}>
+            <span className={styles.span}>
+              {soyFree === true ? "💚 " : "💔"}
+            </span>
+            Soy Free
+          </h4>
         </div>
       </div>
     </div>
