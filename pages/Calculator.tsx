73032480a26dc77657a0c2b3e9165879af4d1ae1
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-30 11:06:54 +0200

    ✨ add calculator page

---
+++ pages/Calculator.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Calculator.module.css";
+
+export default function SplashPage() {
+  return (
+    <div>
+      <div className={styles.logo}>
+        <div className={styles.link}>
+          <a href="/">press here to continue</a>
+        </div>
+      </div>
+    </div>
+  );
+}
