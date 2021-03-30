c56a6f889b3048adf4dd0f60244283900b17acb3
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-30 12:08:51 +0200

    🚧 ✨ add calculator page (WIP)

--- pages/Calculator.tsx
+++ pages/Calculator.tsx
@@ -1,4 +1,6 @@
 import styles from "../styles/Calculator.module.css";
+import Calc from "../components/calculator/calculator";
+import Row from "../components/calculator/rows/row";
 
 export default function SplashPage() {
   return (
@@ -6,6 +8,13 @@ export default function SplashPage() {
       <div className={styles.logo}>
         <div className={styles.link}>
           <a href="/">press here to continue</a>
+          <h1>Calculate :)</h1>
+          <Calc label="calc" />
+          <div>
+            <Row label="row" />
+            <div>=</div>
+            <Row label="row" />
+          </div>
         </div>
       </div>
     </div>
