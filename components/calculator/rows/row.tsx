5735c5c6a0a1f33c9687e2a989045d7dfa91c0cc
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-30 12:32:46 +0200

    ✨ fetch data with useEffect

--- components/calculator/rows/row.tsx
+++ components/calculator/rows/row.tsx
@@ -1,10 +1,18 @@
 import styles from "./row.module.css";
+import { useEffect } from "react";
 
 export type RowProps = {
   option: string;
 };
+const APIData = `/api/ingredients/`;
 
 function Row({ option }: RowProps) {
+  useEffect(() => {
+    fetch(APIData)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <input className={styles.input} type="number" placeholder="here" />
