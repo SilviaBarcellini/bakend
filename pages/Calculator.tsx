e8205699f867ed6b8389ccbc53b66f669419597b
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-31 11:47:44 +0200

    ✨ options conditional rendering +  🚚 move hooks

--- pages/Calculator.tsx
+++ pages/Calculator.tsx
@@ -1,22 +1,35 @@
 import styles from "../styles/Calculator.module.css";
-import Calc from "../components/calculator/calculator";
 import Row from "../components/calculator/rows/row";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+import { useState, useEffect } from "react";
 
-export default function SplashPage() {
+export default function Calculator() {
+  const [ingOptions, setIngOptions] = useState([]);
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      const response = await fetch(`/api/ingredients/`);
+      const ingOptions = await response.json();
+      setIngOptions(ingOptions);
+    };
+    getIngredients();
+  }, []);
+
   return (
     <div>
-      <div className={styles.logo}>
-        <div className={styles.link}>
-          <a href="/">press here to continue</a>
-          <h1>Calculate :)</h1>
-          <Calc label="calc" />
-          <div>
-            <Row option="option 1" />
+      <Header logo="bakend" imageSrc="/donuts.svg" />
+      <h1>Calculate :)</h1>
+      <div>
+        {ingOptions && (
+          <>
+            <Row options={ingOptions} />
             <div>=</div>
-            <Row option="option 2" />
-          </div>
-        </div>
+            <Row options={ingOptions} />
+          </>
+        )}
       </div>
+      <Footer homeIcon="/home.svg" favsIcon="/heartf.svg" />
     </div>
   );
 }
