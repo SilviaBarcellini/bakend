e8eca9ddbbcd6af3fb4a01b843a686527e353886
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-31 15:09:04 +0200

    ✨ add default option item + ✨ add to- and from-

--- pages/Calculator.tsx
+++ pages/Calculator.tsx
@@ -5,12 +5,19 @@ import { useState, useEffect } from "react";
 
 export default function Calculator() {
   const [ingOptions, setIngOptions] = useState([]);
+  const [fromIng, setFromIng] = useState();
+  const [toIng, setToIng] = useState();
 
   useEffect(() => {
     const getIngredients = async () => {
       const response = await fetch(`/api/ingredients/`);
       const ingOptions = await response.json();
+      const basicIng = ingOptions[1];
+
       setIngOptions(ingOptions);
+      setFromIng(basicIng.name);
+      setToIng(ingOptions);
+      console.log(basicIng.name);
     };
     getIngredients();
   }, []);
@@ -22,9 +29,9 @@ export default function Calculator() {
       <div>
         {ingOptions && (
           <>
-            <Row options={ingOptions} />
+            <Row options={ingOptions} selectedIng={fromIng} />
             <div>=</div>
-            <Row options={ingOptions} />
+            <Row options={ingOptions} selectedIng={toIng} />
           </>
         )}
       </div>
