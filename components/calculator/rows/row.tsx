bf63ea9e5e5f50da8d053f4bc276186d4c425e77
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-03-31 16:37:06 +0200

    ✨ add onChangeEvent

--- components/calculator/rows/row.tsx
+++ components/calculator/rows/row.tsx
@@ -1,16 +1,26 @@
+//import { ChangeEventHandler } from "react";
 import type { Ingredient } from "../../../src/utils/api";
 
 type RowProps = {
   options: Ingredient[];
   selectedIng: string;
+  onChangeIng: string;
+  //onChangeIng: ChangeEventHandler<HTMLSelectElement>;
 };
 
-function Row({ options, selectedIng }: RowProps) {
+function Row({ options, selectedIng, onChangeIng }: RowProps) {
   return (
     <div>
       <div>
         <input type="number" placeholder="type here" />
-        <select value={selectedIng}>
+        <select
+          value={selectedIng}
+          onChange={onChangeIng}
+
+          /* onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            e.target.value;
+          }} */
+        >
           {options.map((ingredient) => {
             return <option key={ingredient._id}>{ingredient.name}</option>;
           })}
