9c77cd190147ac6282032eeb96fc3df546c7a593
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-03 22:35:52 +0200

    ✨ reboot calculator row and add filter

--- components/calculator/rows/row.tsx
+++ components/calculator/rows/row.tsx
@@ -1,18 +1,29 @@
 import type { Ingredient } from "../../../src/utils/api";
+import { getSugars } from "../../../src/utils/api";
 
 type RowProps = {
   options: Ingredient[];
+  selectedIng: string;
+  onSelectChange: (selection: string) => void;
 };
 
-function Row({ options }: RowProps) {
+function Row({ options, selectedIng, onSelectChange }: RowProps) {
   return (
     <div>
       <div>
-        <input type="number" placeholder="type here" />
-        <select>
-          {options.map((ingredient) => {
-            return <option key={ingredient._id}>{ingredient.name}</option>;
-          })}
+        <select
+          value={selectedIng}
+          onChange={(e) => onSelectChange(e.target.value)}
+        >
+          {options
+            .filter((ingredient) => ingredient.family === "Sugars")
+            .map((filteredIngredient) => {
+              return (
+                <option key={filteredIngredient._id}>
+                  {filteredIngredient.name} {/* {ingredient.inst} */}
+                </option>
+              );
+            })}
         </select>
       </div>
     </div>
