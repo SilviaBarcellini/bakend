595346b6322eaabd028a5a873f40c812c7253a30
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-03 23:17:26 +0200

    🐛 fix empty page bug

--- pages/Calculator.tsx
+++ pages/Calculator.tsx
@@ -1,1 +1,15 @@
-import Row from "../components/calculator/rows/row";
+import styles from "../styles/Splash.module.css";
+import Logo from "../components/logo/logo";
+
+export default function SplashPage() {
+  return (
+    <div>
+      <div className={styles.logo}>
+        <Logo logo="bakend" size="big" />
+        <div className={styles.link}>
+          <a href="/">press here to continue</a>
+        </div>
+      </div>
+    </div>
+  );
+}
