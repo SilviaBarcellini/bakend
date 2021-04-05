ab56bdd4a1cefb0ae521fc38fb10d16e9dde85f9
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-05 15:25:04 +0200

    ♻️ reform form

--- components/forms/form.tsx
+++ components/forms/form.tsx
@@ -4,74 +4,56 @@ import styles from "./form.module.css";
 //import { getIngredient } from "../../src/utils/api";
 import CardMain from "../cardMain/cardMain";
 
-type FormProps = {
-  itemRequired: string;
-};
-
 function Form() {
-  const { register, handleSubmit, errors } = useForm<FormProps>();
-  const onSubmit: SubmitHandler<FormProps> = (data) => {
-    alert(data);
-  };
-  const [ingredients, setIngredients] = useState([]);
-  const [filter, setFilter] = useState("");
+  const [ingredient, setIngredient] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const [query, setQuery] = useState("string");
 
   useEffect(() => {
-    getIngredients();
+    getIngredient();
   }, []);
-  const getIngredients = async () => {
-    const response = await fetch(`/api/ingredients/`);
-    const ingredients = await response.json();
-    setIngredients(ingredients);
-    console.log(ingredients);
+
+  const getIngredient = async () => {
+    const response = await fetch(`/api/ingredients/${name}`);
+    const ingredient = await response.json();
+    setIngredient(ingredient);
+    console.log(ingredient);
   };
 
   const updateSearch = (e) => {
     setFilter(e.target.value);
   };
-
-  const filteredIngredients = ingredients.filter((ingredient) =>
-    ingredient.name.startsWith(filter)
-  );
-
+  const getSearch = (e) => {
+    setQuery(filter);
+  };
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-        <input
-          className={styles.input}
-          placeholder="today I am out of ..."
-          name="itemRequired"
-          ref={register({ required: true, minLength: 4 })}
-          type="text"
-          value={filter}
-          onChange={updateSearch}
+      <input
+        className={styles.input}
+        name="itemRequired"
+        type="text"
+        value={filter}
+        onChange={updateSearch}
+      />
+
+      {ingredient.map((ingredient) => (
+        <CardMain
+          key={ingredient.name}
+          name={ingredient.name}
+          image={ingredient.image}
+          fat={ingredient.fat}
+          carbs={ingredient.carbs}
+          protein={ingredient.protein}
+          family={ingredient.family}
+          vegan={ingredient.vegan}
+          vegetarian={ingredient.vegetarian}
+          glutenFree={ingredient.glutenFree}
+          dairyFree={ingredient.dairyFree}
+          nutsFree={ingredient.glutenFree}
+          soyFree={ingredient.soyFree}
         />
-        {errors.itemRequired && <p>This field is required</p>}
-        <button className={styles.button} type="submit">
-          Go!
-        </button>
-      </form>
-      <div className={styles.cardMain}>
-        {filteredIngredients.map((ingredient) => (
-          <CardMain
-            key={ingredient.name}
-            name={ingredient.name}
-            image={ingredient.image}
-            fat={ingredient.fat}
-            carbs={ingredient.carbs}
-            protein={ingredient.protein}
-            family={ingredient.family}
-            vegan={ingredient.vegan}
-            vegetarian={ingredient.vegetarian}
-            glutenFree={ingredient.glutenFree}
-            dairyFree={ingredient.dairyFree}
-            nutsFree={ingredient.glutenFree}
-            soyFree={ingredient.soyFree}
-          />
-        ))}
-      </div>
+      ))}
     </div>
   );
 }
-
 export default Form;
