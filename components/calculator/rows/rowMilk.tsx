17d0f4a074b3893d3489046bc7e4e277753657da
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-06 17:02:07 +0200

    ✨ filter items belonging to Liquids family

---
+++ components/calculator/rows/rowMilk.tsx
@@ -0,0 +1,34 @@
+import type { Ingredient } from "../../../src/utils/api";
+import styles from "./row.module.css";
+
+type RowProps = {
+  options: Ingredient[];
+  selectedIng: string;
+  onSelectChange: (selection: string) => void;
+};
+
+function Row({ options, selectedIng, onSelectChange }: RowProps) {
+  return (
+    <div>
+      <div>
+        <select
+          value={selectedIng}
+          onChange={(e) => onSelectChange(e.target.value)}
+          className={styles.row}
+        >
+          {options
+            .filter((ingredient) => ingredient.family === "Liquids")
+            .map((filteredIngredient) => {
+              return (
+                <option key={filteredIngredient._id}>
+                  {filteredIngredient.name}
+                </option>
+              );
+            })}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Row;
