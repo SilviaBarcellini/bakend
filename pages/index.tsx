31e48a03f5cb023647ea15d6a2a42aa5eaa2cb5b
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-07 23:07:37 +0200

    💄 replace personal favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className={styles.container}>
           <Head>
             <title>Create Next App</title>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/faviconn.ico" />
           </Head>
           <main className={styles.main}></main>
           <div>
