28cad956e58918ba9e6b74320c5d11d81ce53eeb
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-11 18:39:21 +0200

    💄 change footer components order

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import styles from "./footer.module.css";
 import Theme from "../dark/themeBtn";
 import Link from "next/link";
+import Btn from "../btn/btn";
 
 export type FooterProps = {
   homeIcon: string;
@@ -9,12 +10,13 @@ export type FooterProps = {
 function Footer({ homeIcon }: FooterProps) {
   return (
     <footer className={styles.footer}>
-      <Theme />
       <Link href="/">
         <a>
           <img className={styles.home} src={homeIcon} alt="home" />
         </a>
       </Link>
+
+      <Theme />
     </footer>
   );
 }
