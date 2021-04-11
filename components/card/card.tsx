b771124d6ef23116df80d071efec02880adb4099
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-11 18:02:36 +0200

    ✨ add 404 fallback card

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,17 +1,19 @@
 import styles from "./card.module.css";
 
 export type CardProps = {
+  status: string;
   title: string;
   image: string;
 };
 
-function Card({ title, image }: CardProps) {
+function CardNot({ status, title, image }: CardProps) {
   return (
     <div className={styles.card}>
+      <h1 className={styles.status}>{status}</h1>
       <img className={styles.image} src={image} alt="" />
       <h2 className={styles.title}>{title}</h2>
     </div>
   );
 }
 
-export default Card;
+export default CardNot;
