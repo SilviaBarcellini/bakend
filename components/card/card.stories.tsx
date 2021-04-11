97bd7a47474d7f5774f030fcbc529289658bbbb5
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-04-11 18:13:32 +0200

    ✨ add 404 card component stories

--- components/card/card.stories.tsx
+++ components/card/card.stories.tsx
@@ -9,14 +9,9 @@ export default {
 
 const Template: Story<CardProps> = (args) => <Card {...args} />;
 
-export const bananas = Template.bind({});
-bananas.args = {
-  title: "bananas",
-  image: "https://image.flaticon.com/icons/png/128/2909/2909761.png",
-};
-
-export const milk = Template.bind({});
-milk.args = {
-  title: "milk",
-  image: "https://image.flaticon.com/icons/png/128/684/684631.png",
+export const error = Template.bind({});
+error.args = {
+  status: "404",
+  title: "oh nuts, no ingredients found!",
+  image: "/ice-cream.svg",
 };
